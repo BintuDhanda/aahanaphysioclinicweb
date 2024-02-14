d5a601ab390971a8d0b4b7c27bfd0790dd9fc167
BintuDhanda <bn2mehlan510@gmail.com>
2024-02-14 12:17:47 +0530

    Dashboard made simple

--- aahanaphysioclinic/Controllers/DashboardController.cs
+++ aahanaphysioclinic/Controllers/DashboardController.cs
@@ -19,25 +19,5 @@ namespace aahanaphysioclinic.Controllers
             return View();
         }
 
-        public IActionResult Patient(int pageNo = 1, int pageSize = 10)
-        {
-            // Calculate skip count based on pageNo and pageSize for pagination
-            int skipCount = (pageNo - 1) * pageSize;
-
-            // Retrieve patients from the database using Skip() and Take() for pagination
-            var patients = _context.Patients.Skip(skipCount).Take(pageSize).ToList();
-
-            // Pass patients to the view
-            return View(patients);
-        }
-        public IActionResult Patient(Patient patient)
-        {
-            return View();
-        }
-        public IActionResult Encounter()
-        {
-            return View();
-        }
-
     }
 }
