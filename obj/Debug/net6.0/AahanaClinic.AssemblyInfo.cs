84a522e83227dd2f3a4788c7fcd08b0ce26649f1
dharmveerofficial <dharmvirofficial@gmail.com>
2024-11-02 13:10:25 +0530

    Updated

--- obj/Debug/net6.0/AahanaClinic.AssemblyInfo.cs
+++ obj/Debug/net6.0/AahanaClinic.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("AahanaClinic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4ce6d653480227234b8f635d591c0b5d42274ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6edbbc1dd1a5aa01aea4f6a49fc8077e5a23dcf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AahanaClinic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AahanaClinic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
